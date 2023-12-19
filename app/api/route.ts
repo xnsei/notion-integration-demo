7b26c31629b0b28305e26940beba9d80beee2295
Tanmay Narayan Banage <tbanage@j5fkwj5wqm.agoda.local>
2023-12-19 17:38:09 +0530

    updating redirectURI

--- app/api/route.ts
+++ app/api/route.ts
@@ -3,7 +3,7 @@ export const GET = async (req: Request, res: Response) => {
     const OAUTH_CLIENT_ID = "166dcda3-f0de-4629-807e-8ecd3f50778c";
     const OAUTH_CLIENT_SECRET = "secret_POixhAyh7OgUSUKxrdtZAgZAay2M3p4pkQDO1QSByvg";
     const encoded = Buffer.from(`${OAUTH_CLIENT_ID}:${OAUTH_CLIENT_SECRET}`).toString("base64");
-    const redirectUri = "http://localhost:3000/";
+    const redirectUri = "https://notion-integration-1.vercel.app/";
 
     const response = await fetch("https://api.notion.com/v1/oauth/token", {
       method: "POST",
